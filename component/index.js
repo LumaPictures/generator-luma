a6288873b6d82c4be70d5661957faabdeba8b342
Austin Rivas <austinrivas@gmail.com>
2014-05-07 16:17:59 -0700

    fixed typo

--- component/index.js
+++ component/index.js
@@ -21,14 +21,14 @@ Generator.prototype.createComponentFiles = function createComponentFiles() {
   this.writeTemplate('lib/component.server.coffee', path.join( 'lib/' + this.name.toLowerCase() + '.server.coffee'));
   this.writeTemplate('lib/component.component.coffee', path.join( 'lib/' + this.name.toLowerCase() + '.component.coffee'));
 
-  this.writeTemplate('lib/mixins/component.mixin.coffee', path.join( 'lib/mixin/' + this.name.toLowerCase() + '.mixin.coffee' ));
-  this.writeTemplate('lib/mixins/initialize.mixin.coffee', path.join( 'lib/mixin/initialize.mixin.coffee' ));
-  this.writeTemplate('lib/mixins/destroy.mixin.coffee', path.join( 'lib/mixin/destroy.mixin.coffee' ));
-  this.writeTemplate('lib/mixins/debug.mixin.coffee', path.join( 'lib/mixin/debug.mixin.coffee' ));
-  this.writeTemplate('lib/mixins/utility.mixin.coffee', path.join( 'lib/mixin/utility.mixin.coffee' ));
-  this.writeTemplate('lib/mixins/selector.mixin.coffee', path.join( 'lib/mixin/selector.mixin.coffee' ));
-  this.writeTemplate('lib/mixins/options.mixin.coffee', path.join( 'lib/mixin/options.mixin.coffee' ));
-  this.writeTemplate('lib/mixins/template.mixin.coffee', path.join( 'lib/mixin/template.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/component.mixin.coffee', path.join( 'lib/mixins/' + this.name.toLowerCase() + '.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/initialize.mixin.coffee', path.join( 'lib/mixins/initialize.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/destroy.mixin.coffee', path.join( 'lib/mixins/destroy.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/debug.mixin.coffee', path.join( 'lib/mixins/debug.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/utility.mixin.coffee', path.join( 'lib/mixins/utility.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/selector.mixin.coffee', path.join( 'lib/mixins/selector.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/options.mixin.coffee', path.join( 'lib/mixins/options.mixin.coffee' ));
+  this.writeTemplate('lib/mixins/template.mixin.coffee', path.join( 'lib/mixins/template.mixin.coffee' ));
 
   this.writeTemplate('lib/options/component.option.coffee', path.join( 'lib/options/' + this.name.toLowerCase() + '.option.coffee'));
 

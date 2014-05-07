2579e1f21c7bf84050b405836ef5f210cdac9038
austinrivas <austinrivas@gmail.com>
2014-05-07 01:40:52 -0700

    refactor package templates into package

--- package/index.js
+++ package/index.js
@@ -7,9 +7,9 @@ var scriptBase = require('../script-base.js');
 var Generator = module.exports = function Generator() {
   scriptBase.apply(this, arguments);
   // calling NamedBase allows us to retrieve the name argument associated with
-  // yo meteor:view name, and set it to this.name *magic*
+  // yo meteor:package name, and set it to this.name *magic*
   yeoman.generators.NamedBase.apply(this, arguments);
-  this.sourceRoot(path.join(__dirname, '../templates'));
+  this.sourceRoot(path.join(__dirname, 'templates'));
 };
 
 util.inherits(Generator, scriptBase);
@@ -54,31 +54,31 @@ Generator.prototype.askForPackage = function askForPackage() {
 };
 
 Generator.prototype.createPackageFiles = function createPackageFiles() {
-  this.writeTemplate('package/smart.json', path.join('smart.json'));
-  this.writeTemplate('package/package.js', path.join('package.js'));
-  this.writeTemplate('package/README.md', path.join('README.md'));
-  this.writeTemplate('package/LICENSE.md', path.join('LICENSE.md'));
-  this.writeTemplate('package/.travis.yml', path.join('.travis.yml'));
-  this.writeTemplate('package/.groc.json', path.join('.groc.json'));
-  this.writeTemplate('package/.gitignore', path.join('.gitignore'));
-  this.writeTemplate('package/lib/package.coffee', path.join('lib/', this.name.toLowerCase() + '.coffee'));
-  this.writeTemplate('package/tests/package.test.coffee', path.join('tests/', this.name.toLowerCase() + '.test.coffee'));
+  this.writeTemplate('smart.json', path.join('smart.json'));
+  this.writeTemplate('package.js', path.join('package.js'));
+  this.writeTemplate('README.md', path.join('README.md'));
+  this.writeTemplate('LICENSE.md', path.join('LICENSE.md'));
+  this.writeTemplate('.travis.yml', path.join('.travis.yml'));
+  this.writeTemplate('.groc.json', path.join('.groc.json'));
+  this.writeTemplate('.gitignore', path.join('.gitignore'));
+  this.writeTemplate('lib/package.coffee', path.join('lib/', this.name.toLowerCase() + '.coffee'));
+  this.writeTemplate('tests/package.test.coffee', path.join('tests/', this.name.toLowerCase() + '.test.coffee'));
 
-  this.writeTemplate('package/example/smart.json', path.join('example/smart.json'));
-  this.writeTemplate('package/example/routes.coffee', path.join('example/routes.coffee'));
+  this.writeTemplate('example/smart.json', path.join('example/smart.json'));
+  this.writeTemplate('example/routes.coffee', path.join('example/routes.coffee'));
 
-  this.writeTemplate('package/example/.meteor/.gitignore', path.join("example/.meteor/.gitignore"));
-  this.writeTemplate('package/example/.meteor/packages', path.join("example/.meteor/packages"));
+  this.writeTemplate('example/.meteor/.gitignore', path.join("example/.meteor/.gitignore"));
+  this.writeTemplate('example/.meteor/packages', path.join("example/.meteor/packages"));
 
-  this.writeTemplate('package/example/client/index.html', path.join('example/client/index.html'));
-  this.writeTemplate('package/example/client/lib/example.controller.coffee', path.join("example/client/lib/example.controller.coffee"));
-  this.writeTemplate('package/example/client/main.less', path.join("example/client/main.less"));
-  this.writeTemplate('package/example/client/views/pages/home/home.html', path.join("example/client/views/pages/home/home.html"));
-  this.writeTemplate('package/example/client/views/pages/home/home.coffee', path.join("example/client/views/pages/home/home.coffee"));
+  this.writeTemplate('example/client/index.html', path.join('example/client/index.html'));
+  this.writeTemplate('example/client/lib/example.controller.coffee', path.join("example/client/lib/example.controller.coffee"));
+  this.writeTemplate('example/client/main.less', path.join("example/client/main.less"));
+  this.writeTemplate('example/client/views/pages/home/home.html', path.join("example/client/views/pages/home/home.html"));
+  this.writeTemplate('example/client/views/pages/home/home.coffee', path.join("example/client/views/pages/home/home.coffee"));
 
-  this.writeTemplate('package/example/packages/.gitignore', path.join("example/packages/.gitignore"));
+  this.writeTemplate('example/packages/.gitignore', path.join("example/packages/.gitignore"));
 
-  this.writeTemplate('package/example/server/lib/settings.coffee', path.join("example/server/lib/settings.coffee"));
+  this.writeTemplate('example/server/lib/settings.coffee', path.join("example/server/lib/settings.coffee"));
 
-  this.writeTemplate('package/example/public/README.md', path.join("example/public/README.md"));
+  this.writeTemplate('example/public/README.md', path.join("example/public/README.md"));
 };
